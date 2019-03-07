495c6068e038a380e8bd25de65af2a3223a210da
DulceGarcia45 <dulcegarcia4545@gmail.com>
2019-03-06 18:35:44 -0600

    Encapsulamiento de clases

---
+++ Actividad5/Actividad5/Modelo/Carrera.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad5.Modelo
+{
+    class Carrera
+    {
+        private long _duracion;
+        private string _nombre;
+        private string _descripcion;
+
+
+        public long NumeroControl
+        {
+            get
+            {
+                return this._duracion;
+            }
+            set
+            {
+                if (value > 0)
+                    this._duracion = value;
+
+            }
+        }
+        public string Descripcion { get; set; }
+        public string Nombre { get; set; }
+        public long Duracion { get; set; }
+
+    }
+}
+
---
+++ Actividad5/Actividad5/Modelo/Facilitador.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad5.Modelo
+{
+    class Facilitador
+    {
+        private string _profesion;
+        private string _nombre;
+        private byte _edad;
+       
+        public long NumeroControl
+        {
+            get
+            {
+                return this._edad;
+            }
+            set
+            {
+                if (value > 0)
+                    this._edad = value;
+
+            }
+        }
+        public string profesion { get; set; }
+        public string Nombre { get; set; }
+        public byte Edad { get; set; }
+       
+    }
+}
+}
