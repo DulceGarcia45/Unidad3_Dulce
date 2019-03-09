85a3d7d267937ae9618f89fdfdf9813e07e74748
DulceGarcia45 <dulcegarcia4545@gmail.com>
2019-03-09 11:11:03 -0600

    ejemplo liga de fut bol

---
+++ ejemplo 2/ejemplo 2/Campeonato/Jornada.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo_2.Campeonato
+{
+    class Jornada
+    {
+        private int _id;
+        private DateTime _fechaInicio;
+        private DateTime _fechaFin;
+        private bool _isConcluida;
+
+        //constructor
+        public Jornada ()
+        {
+            this._fechaInicio = DateTime.Today;
+            this._isConcluida = false;
+
+        }
+        public Jornada (int id)
+        {
+            this._id = id;
+        }
+            
+        // Metodo Getter's y setter's
+        public int Id { get; set; }
+        public DateTime FechaInicio {
+            get{ return this._fechaInicio; }
+
+            
+                set {
+                if (value >DateTime.Today)
+                {
+                    this._fechaInicio = value;
+                }
+            }
+        }
+        public  DateTime FechaFin {
+            get { return this._fechaFin; }
+            set
+        {
+                DateTime fin = new DateTime(2019,4,8);
+                if (value <= fin)
+                {
+                    this._fechaFin = value;
+                }
+            }
+        }
+        public bool IsConcluida { get; set; }
+
+
+    }
+}
---
+++ ejemplo 2/ejemplo 2/Campeonato/Partido.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo_2.Campeonato
+{
+    class Partido
+    {
+        private int _id;
+        private string _descripcion;
+        private DateTime _fecha;
+        private bool _isFinalizado;
+        private string _observaciones;
+        // asocia con cancha
+        private Cancha _cancha1;
+        private Equipo _local;
+        private Equipo _visistante;
+
+        // se asocia con arbitro
+        private Arbitro _arbitro1;
+        private Arbitro _arbitro2;
+
+        // resultado de la asociacion
+
+        private List<Cancha> _cancha = new List<Cancha>();
+        private List<Arbitro> _arbitro1 = new List<Arbitro>();
+        private List<Arbitro> _arbitro2 = new List<Arbitro>();
+
+        //constructores default
+        public addArbitro (Arbitro A)
+        {
+            this._arbitro1 = A;
+            this._arbitro2 = A;
+
+        }
+        public void addCancha(Cancha C)
+        {
+            this._cancha1 = C;
+        }
+        public Partido ()
+        {
+            this._isFinalizado = false;
+            this._id = 1;
+            Random alea = new Random();
+            int dia = alea.Next(1, 31);
+
+            this._fecha = new DateTime(2019,3,dia,16,0,0);
+        }
+        public Partido(Equipo equipo1, Equipo equipo2)
+        {
+            this._local = equipo1;
+            this._visistante = equipo2;
+
+        }
+        
+
+        ///private Arbitro [] arbitro;
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public bool IsFinalizados { get; set; }
+
+
+        
+    }
+}
